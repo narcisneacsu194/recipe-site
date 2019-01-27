43a016e6698388dbae8caa3faf983228eaee32a4
narcisneacsu194 <narcisneacsu194@gmail.com>
2019-01-27 11:29:38 +0200

    Removed unused methods.

--- src/main/java/com/company/recipes/dao/IngredientDao.java
+++ src/main/java/com/company/recipes/dao/IngredientDao.java
@@ -1,13 +1,8 @@
 package com.company.recipes.dao;
 
 import com.company.recipes.model.Ingredient;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,7 +11,4 @@ import java.util.List;
 public interface IngredientDao extends CrudRepository<Ingredient, Long> {
     @Query("select i from Ingredient i")
     List<Ingredient> findAll();
-
-    @RestResource(rel = "description-containing", path = "containsDescription")
-    Page<Ingredient> findByDescriptionContaining(@Param("description") String description, Pageable page);
 }
