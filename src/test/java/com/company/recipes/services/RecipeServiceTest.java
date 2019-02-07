8026d2919ab428fee9cba562e75c3bc980c20e05
narcisneacsu194 <narcisneacsu194@gmail.com>
2019-02-07 14:08:55 +0200

    Wrote a few more test methods for RecipeServiceTest, in order to reach 100% code coverage.

--- src/test/java/com/company/recipes/services/RecipeServiceTest.java
+++ src/test/java/com/company/recipes/services/RecipeServiceTest.java
@@ -9,6 +9,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -47,5 +48,82 @@ public class RecipeServiceTest {
                 1, 2, 3, 4, "http://placehold.it/350x150"));
         assertThat(service.findOne(1L), instanceOf(Recipe.class));
         verify(dao, times(1)).findOne(anyLong());
+    }
+
+    @Test
+    public void saveOneRecipe_ShouldCallDaoSaveMethodOnce(){
+        Recipe recipe = new Recipe("Name 1", "Description 1", Category.BREAKFAST,
+                1, 2, 3, 4, "http://placehold.it/350x150");
+        service.save(recipe);
+        verify(dao, times(1)).save(recipe);
+    }
+
+    @Test
+    public void saveRecipeList_ShouldCallDaoSaveMethodOnce(){
+        List<Recipe> recipes = new ArrayList<>();
+        Recipe recipe1 = new Recipe("Name 1", "Description 1", Category.BREAKFAST,
+                1, 2, 3, 4, "http://placehold.it/350x150");
+        Recipe recipe2 = new Recipe("Name 2", "Description 2", Category.BREAKFAST,
+                1, 2, 3, 4, "http://placehold.it/350x150");
+        recipes.add(recipe1);recipes.add(recipe2);
+        service.save(recipes);
+
+        verify(dao, times(1)).save(recipes);
+    }
+
+    @Test
+    public void delete_ShouldCallDaoDeleteMethodOnce(){
+        Recipe recipe = new Recipe("Name 1", "Description 1", Category.BREAKFAST,
+                1, 2, 3, 4, "http://placehold.it/350x150");
+        service.delete(recipe);
+        verify(dao, times(1)).delete(recipe);
+    }
+
+    @Test
+    public void findByDescriptionContaining_ShouldReturnAListOfRecipes(){
+        List<Recipe> recipes = new ArrayList<>();
+        Recipe recipe = new Recipe("Name 1", "Description 1", Category.BREAKFAST,
+                1, 2, 3, 4, "http://placehold.it/350x150");
+        recipes.add(recipe);
+        when(dao.findByDescriptionContaining(anyString())).thenReturn(recipes);
+        List<Recipe> returnedRecipes =
+                service.findByDescriptionContaining("Description 1");
+        assertEquals("findByDescriptionContaining should return a list containing one recipe.",
+                1, returnedRecipes.size());
+        assertEquals("findByDescriptionContaining doesn't return the right recipe.",
+                recipe, returnedRecipes.get(0));
+        verify(dao, times(1)).findByDescriptionContaining(anyString());
+    }
+
+    @Test
+    public void findByCategory_ShouldReturnAListOfRecipes(){
+        List<Recipe> recipes = new ArrayList<>();
+        Recipe recipe = new Recipe("Name 1", "Description 1", Category.BREAKFAST,
+                1, 2, 3, 4, "http://placehold.it/350x150");
+        recipes.add(recipe);
+        when(dao.findByCategory(Category.BREAKFAST)).thenReturn(recipes);
+        List<Recipe> returnedRecipes =
+                service.findByCategory("Breakfast");
+        assertEquals("findByCategory should return a list containing one recipe.",
+                1, returnedRecipes.size());
+        assertEquals("findByCategory doesn't return the right recipes.", recipe,
+                returnedRecipes.get(0));
+        verify(dao, times(1)).findByCategory(Category.BREAKFAST);
+    }
+
+    @Test
+    public void findAllFromSpecificUser_ShouldReturnAListOfRecipes(){
+        List<Recipe> recipes = new ArrayList<>();
+        Recipe recipe = new Recipe("Name 1", "Description 1", Category.BREAKFAST,
+                1, 2, 3, 4, "http://placehold.it/350x150");
+        recipes.add(recipe);
+        when(dao.findAllFromSpecificUser()).thenReturn(recipes);
+        List<Recipe> returnedRecipes =
+                service.findAllFromSpecificUser();
+        assertEquals("findAllFromSpecificUser should return a list containing one recipe.", 1,
+                returnedRecipes.size());
+        assertEquals("findAllFromSpecificUser doesn't return the right recipes.",
+                recipe, returnedRecipes.get(0));
+        verify(dao, times(1)).findAllFromSpecificUser();
     }
 }
